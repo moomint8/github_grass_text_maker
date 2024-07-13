6a62b006af5decc10f865f6d6500acdb19c3953f
moomint8 <office05053@gmail.com>
2024-07-13 10:37:00 +0900

    build: create project

---
+++ src/main/java/moomint/toy/github_grass_text_maker/GithubGrassTextMakerApplication.java
@@ -0,0 +1,13 @@
+package moomint.toy.github_grass_text_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGrassTextMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubGrassTextMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moomint/toy/github_grass_text_maker/GithubGrassTextMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package moomint.toy.github_grass_text_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGrassTextMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
