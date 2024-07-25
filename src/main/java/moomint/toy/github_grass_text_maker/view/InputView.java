6661aad0579b71342ce3c10023741f690c2d381b
moomint8 <office05053@gmail.com>
2024-07-25 23:48:45 +0900

    feat(Single Commit): 특정 날짜 커밋 기능 구현
    사용자에게 입력받은 시간에 커밋을 진행합니다.
    이후 입력받은 레포지토리에 해당 커밋을 푸시합니다.

--- src/main/java/moomint/toy/github_grass_text_maker/view/InputView.java
+++ src/main/java/moomint/toy/github_grass_text_maker/view/InputView.java
@@ -30,14 +30,33 @@ public class InputView {
             System.out.println("해당 디렉토리가 존재하지 않아 디렉토리를 생성합니다.");
             commitFileLocation.mkdir();
         }
-        System.out.println("commitFileLocation = " + commitFileLocation);
 
-        // 3. text.txt 파일 생성하고 Git add 및 commit
         File file = new File(commitFileLocation, "text.txt");
         try (FileWriter writer = new FileWriter(file)) {
             writer.write("This is a commit test file.");
         } catch (IOException e) {
             System.out.println("[ERROR] " + e.getMessage());
+        }
+
+        try {
+            runCommand("git init", commitFileLocation);  // 필요시 리포지토리 초기화
+            runCommand("git add text.txt", commitFileLocation);
+            runCommand(String.format("git commit -m \"Initial commit\" --date=\"%s\"", commitDateWithOffset), commitFileLocation);
+
+            if (file.delete()) {
+                runCommand("git add -u", commitFileLocation);
+                runCommand(String.format("git commit -m \"Delete text.txt\" --date=\"%s\"", commitDateWithOffset), commitFileLocation);
+            } else {
+                System.err.println("파일 삭제 실패");
+            }
+
+            runCommand("git remote add origin " + repoUrl, commitFileLocation);
+            runCommand("git branch -M main", commitFileLocation);
+            runCommand("git push -u origin main", commitFileLocation);
+
+            System.out.println("작업이 완료되었습니다.");
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
