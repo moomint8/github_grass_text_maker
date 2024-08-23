b0d0d3cdaeb68240a852556c775aa7557d0dc799
moomint8 <office05053@gmail.com>
2024-08-23 19:38:23 +0900

    feat: Java, Python 선택 추가
    이제 텍스트 파일 뿐만 아니라 자바와 파이썬도 선택할 수 있습니다.

---
+++ src/main/java/moomint/toy/github_grass_text_maker/template/JavaTemplate.java
@@ -0,0 +1,49 @@
+package moomint.toy.github_grass_text_maker.template;
+
+public class JavaTemplate {
+    public static void main(String[] args) {
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+        System.out.println("This is a Template file for GitHub grass.");
+    }
+}
--- src/main/java/moomint/toy/github_grass_text_maker/util/GithubManager.java
+++ src/main/java/moomint/toy/github_grass_text_maker/util/GithubManager.java
@@ -3,6 +3,9 @@ package moomint.toy.github_grass_text_maker.util;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class GithubManager {
 
@@ -14,21 +17,46 @@ public class GithubManager {
             commitFileLocation.mkdir();
         }
 
-        File file = new File(commitFileLocation, "text.txt");
-        try (FileWriter writer = new FileWriter(file)) {
-            writer.write("This is a commit test file.");
-        } catch (IOException e) {
-            System.out.println("[ERROR] " + e.getMessage());
-        }
+        System.out.println("생성할 파일의 타입을 선택하세요.");
+        System.out.println("1. 자바");
+        System.out.println("2. 파이썬");
+        System.out.println("3. 텍스트 파일");
+
+        System.out.print("입력: ");
+        Scanner sc = new Scanner(System.in);
+
+        int selectFileType = sc.nextInt();
 
         try {
+
+            File file;
+            String fileName;
+
+            switch (selectFileType) {
+                case 1 -> {
+                    file = copyFile("src/main/java/moomint/toy/github_grass_text_maker/template/JavaTemplate.java", commitFileLocation);
+                    fileName = "JavaTemplate.java";
+                }
+                case 2 -> {
+                    file = copyFile("src/main/java/moomint/toy/github_grass_text_maker/template/PythonTemplate.py", commitFileLocation);
+                    fileName = "PythonTemplate.py";
+                }
+                case 3 -> {
+                    file = new File(commitFileLocation, "text.txt");
+                    FileWriter writer = new FileWriter(file);
+                    writer.write("This is a commit test file.");
+                    fileName = "text.txt";
+                }
+                default -> throw new RuntimeException("지원하지 않는 언어입니다.");
+            }
+
             runCommand("git init", commitFileLocation);  // 필요시 리포지토리 초기화
-            runCommand("git add text.txt", commitFileLocation);
+            runCommand("git add " + fileName, commitFileLocation);
             runCommand(String.format("git commit -m \"Initial commit\" --date=\"%s\"", commitDate), commitFileLocation);
 
             if (file.delete()) {
                 runCommand("git add -u", commitFileLocation);
-                runCommand(String.format("git commit -m \"Delete text.txt\" --date=\"%s\"", commitDate), commitFileLocation);
+                runCommand(String.format("git commit -m \"Delete " + fileName + "\" --date=\"%s\"", commitDate), commitFileLocation);
             } else {
                 System.err.println("파일 삭제 실패");
             }
@@ -40,6 +68,16 @@ public class GithubManager {
         }
 
         return commitFileLocation;
+    }
+
+    private File copyFile(String sourcePath, File targetDir) throws IOException {
+        Path currentWorkingDir = Path.of("").toAbsolutePath();
+        Path source = currentWorkingDir.resolve(sourcePath); // 현재 작업 디렉토리에 상대 경로 결합
+        File targetFile = new File(targetDir, source.getFileName().toString());
+
+        Files.copy(source, targetFile.toPath());
+
+        return targetFile;
     }
 
     public void pusher(String repoUrl, File commitFileLocation) {
